562f65c6c1acc23e6633d4d53dce60ad069bd5b1
Gabriel Braguin <gabriel.braguin@hotmail.com.br>
2021-05-25 00:55:20 -0300

    material icons + ui refresh

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,10 +6,6 @@ function Footer() {
     return <footer id="footer">
 
         <div class="container-fluid">
-            {/* <i class="icon-footer fab fa-twitter"></i>
-            <i class="icon-footer fab fa-facebook-f"></i>
-            <i class="icon-footer fab fa-instagram"></i>
-            <i class="icon-footer fas fa-envelope"></i> */}
             <p>© Copyright {currentYear} Keeper</p>
         </div>
     </footer>
